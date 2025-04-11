d03b08bfe0e1ffa35c65fda6d16bb3b33db2a2d5
kyawsawshein <xealkssshein@gmail.com>
2025-04-12 00:34:12 +0700

    Chapter 6 permissions

--- backend/todo_project/urls.py
+++ backend/todo_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/', include('todos.urls')),
     path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
