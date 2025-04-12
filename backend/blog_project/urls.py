019b390af5e457a54127aa2c93e5993145449c60
kyawsawshein <xealkssshein@gmail.com>
2025-04-13 03:15:37 +0700

    Chapter 9 Schema

--- backend/blog_project/urls.py
+++ backend/blog_project/urls.py
@@ -14,13 +14,36 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+from drf_yasg import openapi
+
+
+API_TITLE = "Blog API"
+API_DESCRIPTION = "A Web API for createing and editin blog posts."
+
+schema_view = get_schema_view(
+    openapi.Info(title=API_TITLE, default_version="v1", description=API_DESCRIPTION),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('posts.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/rest-auth/', include('dj_rest_auth.urls')),
-    path('api/v1/rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("posts.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/v1/rest-auth/", include("dj_rest_auth.urls")),
+    path("api/v1/rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    # path("docs/", include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+    # path("schema/", schema_view),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
